2a7689848611ce0bafe06c73b7de0566c4d87fea
Danya_Vid <danilviduykov@gmail.com>
2022-06-13 20:57:05 +0500

    settings-page + refactor

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
 import './Settings.scss'
 
-import { faAngleLeft, faAngleRight, faCloudUploadAlt, faUsersViewfinder } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft, faAngleRight, faArrowDownShortWide, faChartPie, faCloud, faMoneyBill, faSpellCheck, faUsersViewfinder } from "@fortawesome/free-solid-svg-icons";
+import { faMoon, faSquare, faSun } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Switch } from 'antd';
 import "antd/dist/antd.css";
+
 import Button from "../../components/Button";
 
 const Settings: FC = () => {
@@ -13,70 +15,71 @@ const Settings: FC = () => {
             <div className="settings__header">
                 <div className="settings__top">
                     <button className="settings__arrow-back"><FontAwesomeIcon icon={faAngleLeft} /></button>
-                    <p className="settings__page-name">Settings</p>
+                    <p className="settings__page-name font-body-large">Settings</p>
                 </div>
                 <div className="settings__user">
                     <div className="settings__img">
                         <img src="" alt="" />
                     </div>
-                    <p className="settings__user-name">John Doe</p>
-                    <p className="settings__mail" >j.doe@gmail.com</p>
+                    <p className="settings__user-name font-headline-4" >John Doe</p>
+                    <p className="settings__mail font-body-small" >j.doe@gmail.com</p>
                     <Button className="settings__btn-edit" type="submit" btnType="Secondary" >Edit profile</Button>
                 </div>
             </div>
             <ul className="settings__category">
                 <li className="settings__category-item">
-                    <h4 className="settings__category-title" >General</h4>
-                    <ul className="settings__list">
+                    <h4 className="settings__category-title font-headline-2" >General</h4>
+                    <ul className="settings__list border">
                         <li className="settings__item">
                             <FontAwesomeIcon className="settings__item-icon" icon={faUsersViewfinder} />
-                            <p className="settings__item-name">Security</p>
-                            <p className="settings__item-description">FaceID <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
+                            <p className="settings__item-name font-headline-2">Security</p>
+                            <p className="settings__item-description font-body-small">FaceID <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
                         </li>
                         <li className="settings__item">
-                            <FontAwesomeIcon className="settings__item-icon" icon={faCloudUploadAlt} />
-                            <p className="settings__item-name">iCloud Sync</p>
+                            <FontAwesomeIcon className="settings__item-icon" icon={faCloud} />
+                            <p className="settings__item-name font-headline-2">iCloud Sync</p>
                             <Switch defaultChecked />
                         </li>
                     </ul>
                 </li>
                 <li className="settings__category-item">
-                    <h4 className="settings__category-title" >My subscriptions</h4>
-                    <ul className="settings__list">
+                    <h4 className="settings__category-title font-headline-2" >My subscriptions</h4>
+                    <ul className="settings__list border">
                         <li className="settings__item">
-                            <FontAwesomeIcon className="settings__item-icon" icon={faUsersViewfinder} />
-                            <p className="settings__item-name">Security</p>
-                            <p className="settings__item-description">FaceID <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
+                            <FontAwesomeIcon className="settings__item-icon" icon={faArrowDownShortWide} />
+                            <p className="settings__item-name font-headline-2">Sorting</p>
+                            <p className="settings__item-description font-body-small">Date <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
                         </li>
                         <li className="settings__item">
-                            <FontAwesomeIcon className="settings__item-icon" icon={faUsersViewfinder} />
-                            <p className="settings__item-name">Security</p>
-                            <p className="settings__item-description">FaceID <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
+                            <FontAwesomeIcon className="settings__item-icon" icon={faChartPie} />
+                            <p className="settings__item-name font-headline-2">Summary</p>
+                            <p className="settings__item-description font-body-small">Average <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
                         </li>
                         <li className="settings__item">
-                            <FontAwesomeIcon className="settings__item-icon" icon={faUsersViewfinder} />
-                            <p className="settings__item-name">Security</p>
-                            <p className="settings__item-description">FaceID <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
+                            <FontAwesomeIcon className="settings__item-icon" icon={faMoneyBill} />
+                            <p className="settings__item-name font-headline-2">Default currency</p>
+                            <p className="settings__item-description font-body-small">USD ($) <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
                         </li>
                     </ul>
                 </li>
-                <li className="settings__category-item">
-                    <h4 className="settings__category-title" >Appearance</h4>
-                    <ul className="settings__list">
+                <li className="settings__category-item" >
+                    <h4 className="settings__category-title font-headline-2">Appearance</h4>
+                    <ul className="settings__list border">
                         <li className="settings__item">
-                            <FontAwesomeIcon className="settings__item-icon" icon={faUsersViewfinder} />
-                            <p className="settings__item-name">Security</p>
-                            <p className="settings__item-description">FaceID <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
+                            <FontAwesomeIcon className="settings__item-icon" icon={faSquare} />
+                            <p className="settings__item-name font-headline-2">App icon</p>
+                            <p className="settings__item-description font-body-small">Default <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
                         </li>
                         <li className="settings__item">
-                            <FontAwesomeIcon className="settings__item-icon" icon={faUsersViewfinder} />
-                            <p className="settings__item-name">Security</p>
-                            <p className="settings__item-description">FaceID <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
+                            <FontAwesomeIcon className="settings__item-icon" icon={faSun} />
+                            {/* <FontAwesomeIcon className="settings__item-icon" icon={faMoon} /> */}
+                            <p className="settings__item-name font-headline-2">Theme</p>
+                            <p className="settings__item-description font-body-small">Dark <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
                         </li>
                         <li className="settings__item">
-                            <FontAwesomeIcon className="settings__item-icon" icon={faUsersViewfinder} />
-                            <p className="settings__item-name">Security</p>
-                            <p className="settings__item-description">FaceID <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
+                            <FontAwesomeIcon className="settings__item-icon" icon={faSpellCheck} />
+                            <p className="settings__item-name font-headline-2">Font</p>
+                            <p className="settings__item-description font-body-small">Inter <FontAwesomeIcon className="angle-right" icon={faAngleRight} /></p>
                         </li>
                     </ul>
                 </li>
--- src/pages/SubscriptionInfo/SubscriptionInfo.tsx
+++ src/pages/SubscriptionInfo/SubscriptionInfo.tsx
@@ -11,39 +11,39 @@ const SubscriptionInfo: FC = () => {
             <div className="sub__header">
                 <div className="sub__actions">
                     <button className="sub__arrow-back"><FontAwesomeIcon icon={faAngleLeft} /></button>
-                    <p className="sub__page-name">Subscription info</p>
+                    <p className="sub__page-name font-body-large">Subscription info</p>
                     <button className="sub__btn-delete"><FontAwesomeIcon icon={faTrashCan} /></button>
                 </div>
                 <div className="sub__img">
                     <img src="" alt="" />
                 </div>
-                <h2 className="sub__title">Spotify</h2>
-                <p className="sub__price">$5.99</p>
+                <h2 className="sub__title font-headline-6">Spotify</h2>
+                <p className="sub__price font-headline-4">$5.99</p>
             </div>
             <div className="sub__info">
-                <ol className="sub__settings">
+                <ol className="sub__settings border">
                     <li className="sub__settings-item">
-                        <p className="sub__settings-name">Name</p>
-                        <p className="sub__settings-description">Spotify <FontAwesomeIcon icon={faAngleRight} /></p>
+                        <p className="sub__settings-name font-headline-2">Name</p>
+                        <p className="sub__settings-description font-body-small">Spotify <FontAwesomeIcon icon={faAngleRight} /></p>
                     </li>
                     <li className="sub__settings-item">
-                        <p className="sub__settings-name">Description</p>
-                        <p className="sub__settings-description">Music app <FontAwesomeIcon icon={faAngleRight} /></p>
+                        <p className="sub__settings-name font-headline-2">Description</p>
+                        <p className="sub__settings-description font-body-small">Music app <FontAwesomeIcon icon={faAngleRight} /></p>
                     </li>
                     <li className="sub__settings-item">
-                        <p className="sub__settings-name">Category</p>
-                        <p className="sub__settings-description">Enterteintment <FontAwesomeIcon icon={faAngleRight} /></p>
+                        <p className="sub__settings-name font-headline-2">Category</p>
+                        <p className="sub__settings-description font-body-small">Enterteintment <FontAwesomeIcon icon={faAngleRight} /></p>
                     </li>
                     <li className="sub__settings-item">
-                        <p className="sub__settings-name">First payment</p>
-                        <p className="sub__settings-description">08.01.2022 <FontAwesomeIcon icon={faAngleRight} /></p>
+                        <p className="sub__settings-name font-headline-2">First payment</p>
+                        <p className="sub__settings-description font-body-small">08.01.2022 <FontAwesomeIcon icon={faAngleRight} /></p>
                     </li>
                     <li className="sub__settings-item">
-                        <p className="sub__settings-name">Currency</p>
-                        <p className="sub__settings-description">USD ($) <FontAwesomeIcon icon={faAngleRight} /></p>
+                        <p className="sub__settings-name font-headline-2">Currency</p>
+                        <p className="sub__settings-description font-body-small">USD ($) <FontAwesomeIcon icon={faAngleRight} /></p>
                     </li>
                 </ol>
-                <Button className="sub__btn-save" type="submit" btnType="Secondary">Save</Button>
+                <Button className="sub__btn-save border" type="submit" btnType="Secondary">Save</Button>
             </div>
         </div>
     )
