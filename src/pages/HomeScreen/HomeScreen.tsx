c039ae0049389498e3364ccb1b5b9d93a9dc69f7
Danya_Vid <danilviduykov@gmail.com>
2022-06-19 18:17:21 +0500

    did indicator total price

--- src/pages/HomeScreen/HomeScreen.tsx
+++ src/pages/HomeScreen/HomeScreen.tsx
@@ -9,7 +9,16 @@ const HomeScreen: FC = () => {
             <div className="home__counter">
                 <div className="home__total">
                     <div className="home__indicator">
-                        indicator
+                        <svg className="spinner" viewBox="0 0 100 100">
+                            <radialGradient id="linearColors2" x1="0.5" y1="0" x2="0.5" y2="1">
+                                <stop offset="0%" stopColor="#FFFFFF"></stop>
+                                <stop offset="43.25%" stopColor="#ffbf9b"></stop>
+                                <stop offset="100%" stopColor="#FF7966"></stop>
+                            </radialGradient>
+                            <circle className="outer" cx="55" cy="55" r="50" fill="none" strokeWidth="4"></circle>
+                            <circle className="middle" cx="55" cy="55" r="50" fill="none" strokeWidth="3"></circle>
+                            <circle className="inner" strokeDasharray={`0 360`} strokeDashoffset={'236'} cx="55" cy="55" r="50" fill="none" strokeWidth="4"></circle>
+                        </svg>
                     </div>
                     <img className="home__logo" src={logo} alt="logo" />
                     <p className="home__total-price ft-7">$1,235</p>
